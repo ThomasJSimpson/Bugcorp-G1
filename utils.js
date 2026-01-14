77426a9d0026ec1842dbd419a54988218887f488
Antares34 <50072129+Antares34@users.noreply.github.com>
2026-01-14 16:59:38 +0100

    journée du mercredi

--- utils.js
+++
@@ -1,6 +0,0 @@
-export async function login(page, username, password) {
-  await page.goto("url_of_your_login_page");
-  await page.fill("#username", username);
-  await page.fill("#password", password);
-  await page.click('button[type="submit"]');
-}
