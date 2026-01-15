0bd039a7d036901d941266bd586eda7f797359d4
Antares34 <50072129+Antares34@users.noreply.github.com>
2026-01-15 16:36:06 +0100

    test-page-acceuil-final

---
+++ tests/Eric/Test-page-acceuil.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Test lien page d'acceuil", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+  test("test", async ({ page }) => {
+    await page.getByTestId("nav-directory").click();
+    await page.getByRole("heading", { name: "L'Annuaire Enterprise" }).click();
+    await page.getByTestId("nav-home").click();
+    await page.getByRole("button", { name: "Pirater le Système" }).click();
+    await page.getByRole("heading", { name: "Accès Restreint" }).click();
+  });
+});
