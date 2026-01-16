baab87459ae38805c52d6d7c358987f3415efccf
Thomas Berenger <berenger.thomas@gmail.com>
2026-01-16 12:04:39 +0100

    test end branch thom

--- tests/Thomas/annuaire.spec.js
+++ tests/Thomas/annuaire.spec.js
@@ -1,6 +1,5 @@
-// @ts-check
 import { test, expect } from "@playwright/test";
-import { goToAnnuaire, getData } from "./utils/functions.js";
+import { goToAnnuaire, getData, checkNextRole, getCurrentSavings, getNameRolesUI, stringToNb, isDescending, getCellsBugs, deleteSteps } from "./utils/functions.js";
 
 test('[US01] - Parcours "Promotion" : Recherche + filtre + promotion', async ({ page }) => {
   await goToAnnuaire(page);
@@ -9,9 +8,7 @@ test('[US01] - Parcours "Promotion" : Recherche + filtre + promotion', async ({ 
   console.log(data);
 
   await page.getByTestId("search-input").fill("Jean");
-  // Localise tous les élements avec leur id commençant par "cell-name-" et on stock dans un tableau
 
-  // On extrait tous les textes d'un coup
   const names = await page.locator('[id^="cell-name-"]').allInnerTexts();
   // names = ["Jean Bon", "Jean Neymar"]
   for (const name of names) {
@@ -24,4 +21,117 @@ test('[US01] - Parcours "Promotion" : Recherche + filtre + promotion', async ({ 
   for (const depart of departs) {
     expect(depart).toContain("RH");
   }
+  const currentRole = await page.locator("#cell-role-1005").innerText();
+  let titleAction = await page.getByTestId("promote-btn-1005").getAttribute("title");
+  const currentSavings = data.savings.currentSavings;
+  let nextCurrentSavings = 0;
+  if (titleAction) {
+    titleAction = titleAction.toLowerCase();
+    nextCurrentSavings = currentSavings + data.legendSavings[titleAction].numberValue;
+  }
+  const nextRole = checkNextRole(titleAction, currentRole, data.legendRoles);
+  console.log(currentRole, nextRole);
+
+  await page.getByTestId("promote-btn-1005").click();
+
+  //Vérif nex role + budjet
+  await expect(page.locator("#cell-role-1005")).toHaveText(nextRole);
+  const actualValue = await getCurrentSavings(page);
+  expect(actualValue).toBe(nextCurrentSavings);
+});
+
+test('[US02] - Parcours "Rétrogradation" : filtres + tri  + sélection multiple + rétrogradation', async ({ page }) => {
+  await goToAnnuaire(page);
+  const data = await getData(page);
+  await page.getByTestId("dept-filter").selectOption("Juridique");
+  const departs = await page.locator('[id^="cell-dept-"]').allInnerTexts();
+  for (const depart of departs) {
+    expect(depart).toContain("Juridique");
+  }
+  await page.getByTestId("status-filter").selectOption("Absent");
+  const allStatus = await page.locator('[id^="cell-status-"]').allInnerTexts();
+  console.log(allStatus);
+  for (const status of allStatus) {
+    expect(status).toContain("Absent");
+  }
+
+  let currentRolesNames = await getNameRolesUI(page);
+  console.log(currentRolesNames);
+
+  let accCurrentSavings = data.savings.currentSavings;
+
+  const allCheckboxes = await page.locator('[id^="checkbox-"]:not([id="checkbox-select-all"])').all();
+
+  // On boucle dessus pour les cocher une par une
+  for (const checkbox of allCheckboxes) {
+    await checkbox.check();
+  }
+
+  let titleAction = await page.locator("#btn-bulk-demote").innerText();
+  // let titleAction = await page.locator("#btn-bulk-promote").innerText();
+
+  console.log(titleAction);
+  if (titleAction) {
+    titleAction = titleAction.toLowerCase().trim();
+  }
+  console.log(titleAction);
+
+  await page.locator("#btn-bulk-demote").click();
+
+  let newRolesNames = await getNameRolesUI(page);
+  console.log(newRolesNames);
+  let roleChangesNumber = 0;
+  for (const nom in currentRolesNames) {
+    const exRole = currentRolesNames[nom];
+    const expectedRole = checkNextRole(titleAction, exRole, data.legendRoles);
+    const currentRole = newRolesNames[nom];
+
+    // Cas limite : Si l'employé était déjà au min/max
+    if (exRole === expectedRole) {
+      console.log(`${nom} était déjà au grade limite (${exRole}). Aucun changement attendu.`);
+    } else {
+      accCurrentSavings += data.legendSavings[titleAction].numberValue;
+      roleChangesNumber++;
+    }
+    // Vérification
+    expect(currentRole).toBe(expectedRole);
+  }
+  const expectedCurrentSavings = roleChangesNumber * data.legendSavings[titleAction].numberValue;
+  expect(accCurrentSavings).toBe(expectedCurrentSavings);
+  console.log(accCurrentSavings, expectedCurrentSavings);
+});
+
+test('[US03] - Parcours "Licenciement" : filtre + tri + sélection multiple + licenciement.', async ({ page }) => {
+  await goToAnnuaire(page);
+  const data = await getData(page);
+  await page.getByTestId("status-filter").selectOption("Sur la sellette");
+  const allStatus = await page.locator('[id^="cell-status-"]').allInnerTexts();
+  console.log(allStatus);
+  for (const status of allStatus) {
+    expect(status).toContain("Sur la sellette");
+  }
+
+  // Tentative de tri "Descendant"
+  let cellsBugsNb = await getCellsBugs(page);
+
+  while (!isDescending(cellsBugsNb)) {
+    await page.getByTestId("header-bugs").click();
+    cellsBugsNb = await getCellsBugs(page);
+  }
+  console.log("c bon c'est trié!");
+
+  const targetSavings = data.savings.targetSavings;
+  const firedValue = data.legendSavings["virer"].numberValue;
+  let nbToFired = Math.ceil(targetSavings / firedValue);
+  console.log(targetSavings);
+  console.log(firedValue);
+  console.log(nbToFired);
+  nbToFired > 10 && (await page.getByTestId("items-per-page").selectOption("20"));
+  const allCheckboxes = await page.locator('[id^="checkbox-"]:not([id="checkbox-select-all"])').all();
+  console.log(allCheckboxes);
+  for (let i = 0; i < nbToFired; i++) {
+    await allCheckboxes[i].check();
+  }
+  await page.locator("#btn-bulk-fire").click();
+  await deleteSteps(page);
 });
--- tests/Thomas/utils/functions.js
+++ tests/Thomas/utils/functions.js
@@ -7,15 +7,18 @@ export async function goToAnnuaire(page) {
   await expect(page.getByRole("heading", { name: "L'Annuaire Enterprise" })).toBeVisible();
 }
 
-function stringToNb(string) {
+export function stringToNb(string) {
   return parseInt(string.replace(/[^\d-]/g, ""), 10);
 }
+
 async function getLegendRoles(page) {
   await page.getByTestId("legend-btn").click();
   await expect(page.getByRole("heading", { name: "Hiérarchie des Rôles" })).toBeVisible();
   const roles = await page.locator('[id^="list-item-role-"] h4').allInnerTexts();
+  await page.getByTestId("modal-confirm-btn").click();
   return roles;
 }
+
 async function getTargetSavings(page) {
   const targetValueTxt = await page.locator("#target-savings-value").innerText();
   const targetValue = stringToNb(targetValueTxt);
@@ -25,7 +28,6 @@ async function getTargetSavings(page) {
 
 async function getLegendSavings(page) {
   const rawTexts = await page.locator("#legend-savings > span").allInnerTexts();
-
   const savingsData = {};
 
   for (const text of rawTexts) {
@@ -34,12 +36,12 @@ async function getLegendSavings(page) {
     // rawValue = " -15 000 €"
 
     // Nettoyage
-    const numberValue = stringToNb(rawValue);
     const key = label.trim().toLowerCase();
+    const numberValue = stringToNb(rawValue);
 
     savingsData[key] = {
-      originalString: rawValue.trim(), // "-15 000 €"
-      numberValue: numberValue, // -15000
+      originalString: rawValue.trim(), // "-15 000 €" string
+      numberValue: numberValue, // -15000 number
     };
   }
   console.log(savingsData);
@@ -53,11 +55,64 @@ export async function getCurrentSavings(page) {
 }
 
 export async function getData(page) {
-  const legendRoles = await getLegendRoles(page);
   const legendSavings = await getLegendSavings(page);
   const targetSavings = await getTargetSavings(page);
   const currentSavings = await getCurrentSavings(page);
+  const legendRoles = await getLegendRoles(page);
   const data = { legendRoles: legendRoles, legendSavings: legendSavings, savings: { currentSavings: currentSavings, targetSavings: targetSavings } };
   console.log(data);
   return data;
+}
+export function checkNextRole(titleAction, currentRole, legendRoles) {
+  let nextRole = "";
+  if (titleAction === "promouvoir") {
+    if (legendRoles.indexOf(currentRole) === legendRoles.length - 1) {
+      nextRole = currentRole;
+    } else {
+      nextRole = legendRoles[legendRoles.indexOf(currentRole) + 1];
+    }
+  } else if (titleAction === "rétrograder") {
+    if (legendRoles.indexOf(currentRole) === 0) {
+      nextRole = currentRole;
+    } else {
+      nextRole = legendRoles[legendRoles.indexOf(currentRole) - 1];
+    }
+  } else {
+    throw new Error(`Titre inattendu : ${titleAction}`);
+  }
+  return nextRole;
+}
+
+export async function getNameRolesUI(page) {
+  const allName = await page.locator('[id^="cell-name-"]').allInnerTexts();
+  const allRoles = await page.locator('[id^="cell-role-"]').allInnerTexts();
+
+  const allRolesNames = allName.reduce((acc, nom, i) => {
+    acc[nom] = allRoles[i];
+    return acc;
+  }, {});
+  return allRolesNames;
+}
+export function isDescending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export async function getCellsBugs(page) {
+  let cellsBugs = await page.locator('[id^="cell-bugs-"]').allInnerTexts();
+  const cellsBugsNb = cellsBugs.map(stringToNb);
+  return cellsBugsNb;
+}
+export async function deleteSteps(page) {
+  await page.locator("#btn-confirm-step1").click();
+  await expect(page.getByRole("heading", { name: "Ils ont des familles" })).toBeVisible();
+  await page.locator("#btn-confirm-step2").click();
+  await expect(page.getByRole("heading", { name: "Confirmation Ultime" })).toBeVisible();
+  await page.locator("#input-confirm-delete").fill("DELETE");
+  await page.locator("#btn-confirm-delete").click();
+  await expect(page.locator("#goal-reached-badge")).toContainText("Objectif Atteint");
 }
