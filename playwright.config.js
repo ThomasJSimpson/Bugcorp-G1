0e7e56e1b6b80f9524504cd0aa446ac3e03f99cd
Antares34 <50072129+Antares34@users.noreply.github.com>
2026-01-27 14:58:58 +0100

    commit sans shell

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    /*  {
+    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
